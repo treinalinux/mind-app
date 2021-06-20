c81da90c6e71b49675ac7d4498beba75888c37c5
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-20 13:13:56 -0300

    Bug - Added validates true and unique on tag name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_152916) do
+ActiveRecord::Schema.define(version: 2021_06_20_155932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_152916) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

92d26a3623624e0cf72fcaa8ef94c310ee2ad057
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-20 20:26:30 -0300

    CRUD - Added list content

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# class ContentsController
+class ContentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @contents = current_user.contents
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # get 'home/index'
   get 'home', to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :contents
 end

6ea7b20b6b0a8a93e36fb2c2413e648e1fe6a37b
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-21 06:54:53 -0300

    config javascript and css for select2 and jquery

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # README
 
+
+## Select2 was added for work with tags
+
+```bash
+yarn add select2 jquery
+```
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import $ from 'jquery'
+import 'select2'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+  $('.select2').select2({
+    tags: true,
+    tokenSeparators: [',', ' ']
+  })
+})

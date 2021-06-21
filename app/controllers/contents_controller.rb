691193f01f52e1cc9961faa89884e7aa65eae8f7
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-20 22:36:35 -0300

    CRUD - Added create of form for new content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -9,6 +9,22 @@ class ContentsController < ApplicationController
   end
 
   def new
-    @contents = Content.new
+    @content = Content.new
+  end
+
+  def create
+    @content = current_user.contents.build(content_params)
+
+    if @content.save
+      redirect_to contents_path, notice: 'Content successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :description)
   end
 end
